69ef7edf857e23ca46ed2cb53b7cb949c47c222b
̷M̷i̷g̷u̷l̷ ̷G̷.̷l̷d̷s̷m̷ ̷D̷'̷a̷r̷t̷ <miguel.gledesma.duarte@gmail.com>
2022-03-04 04:51:25 -0600

    Tutorial Rails 7, ver Concerns

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  
+  has_many :comments
+
+  validates :title, presence: true
+  validates :body, presence: true #, length: { minimum:10 }
+  validates :description, presence: true #, length: { minimum:10 }
 end
