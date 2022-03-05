5516510cdf3314277dae46332559cff2e69afac0
̷M̷i̷g̷u̷l̷ ̷G̷.̷l̷d̷s̷m̷ ̷D̷'̷a̷r̷t̷ <miguel.gledesma.duarte@gmail.com>
2022-03-04 19:38:15 -0600

    Modulo Visible creado

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,11 @@
 class Article < ApplicationRecord
   
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true
-  validates :body, presence: true #, length: { minimum:10 }
-  validates :description, presence: true #, length: { minimum:10 }
+  validates :body, presence: true, length: { minimum:10 }
+  validates :description, presence: true, length: { minimum:10 }
+
 end
